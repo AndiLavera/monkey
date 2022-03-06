1a43abccebb734c83d3a93ee7e02fbc907f40f1a
Andi Crotwell <awcrotwell@gmail.com>
2022-03-05 21:06:03 -0500

    lexer 1.5 - Repl

---
+++ repl/repl.go
@@ -0,0 +1,38 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+const EXIT = "exit"
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		if line == EXIT {
+			return
+		}
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
